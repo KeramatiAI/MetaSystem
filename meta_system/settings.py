2ee515141867afc6e44d07076b48ec1b4eca36d1
davoudkeramati <davoud.keramati@gmail.com>
2025-05-06 16:12:25 +0330

    Added web interface for defining entities, fields, and relations

--- meta_system/settings.py
+++ meta_system/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "meta_system.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [BASE_DIR / 'templates'],  # اضافه کردن پوشه templates
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
